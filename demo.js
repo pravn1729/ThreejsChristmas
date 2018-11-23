c10e2b49c348db4332d04a3038e99ff0e687fdcd
Praveen <praveen@aeries.io>
2018-11-23 16:10:44 +0530

    added music

--- demo.js
+++ demo.js
@@ -39,6 +39,22 @@ function init()
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 1000 );
 	//camera.position.z = 1;
+	
+	// create an AudioListener and add it to the camera
+	var listener = new THREE.AudioListener();
+	camera.add( listener );
+
+	// create a global audio source
+	var sound = new THREE.Audio( listener );
+
+	// load a sound and set it as the Audio object's buffer
+	var audioLoader = new THREE.AudioLoader();
+	audioLoader.load( 'instrument.mp3', function( buffer ) {
+		sound.setBuffer( buffer );
+		sound.setLoop( true );
+		sound.setVolume( 0.5 );
+		sound.play();
+	});
 
 	scene = new THREE.Scene();
 
